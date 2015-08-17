92bcdec360b8cc33411689793d8242e243374eec
Jose C Fernandez <jfernandez700@gmail.com>
2015-08-17 15:24:17 -0400

    A little refactoring, all green

--- lib/human.rb
+++ lib/human.rb
@@ -6,5 +6,22 @@ module Human
       @tummy = tummy
       @state = state
     end
+
+    def goes_and_get_coffee
+      @state = 'awake'
+    end
+    def goes_and_eat_breakfast
+      @tummy = 'full'
+    end
   end
 end
+
+
+  @new_human = Human::Person.new
+  p @new_human.class
+  print "Person is #{@new_human.state} in the morning\n"
+  print "Person is #{@new_human.tummy} in the morning\n"
+  @new_human.goes_and_get_coffee
+  print "Person is #{@new_human.state} in the morning\n"
+  @new_human.goes_and_eat_breakfast
+  print "Person is #{@new_human.tummy} in the morning\n"
--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -1,15 +1,42 @@
 require 'spec_helper'
 
 describe Human::Person do
-  context 'Before breakfast a Person' do
   before(:each) do
     @new_human = Human::Person.new
   end
-    it 'should be hungry' do
+
+  context 'Before breakfast a Person' do
+    it 'should be hungry'do
       expect(@new_human.tummy).to eq('hungry')
     end
+
     it 'should be sleepy' do
       expect(@new_human.state).to eq('sleepy')
+    end
+
+    it 'should not be awake' do
+      expect(@new_human.state).not_to eq('awake')
+    end
+
+    it 'should not be full' do
+      expect(@new_human.tummy).not_to eq('full')
+    end
+  end
+
+
+  context 'After breakfast a Person' do
+    describe '#goes_and_get_coffee' do
+      it 'should be awake after drinking coffee' do
+        @new_human.goes_and_get_coffee
+        expect(@new_human.state).to eq('awake')
+      end
+    end
+
+    describe '#goes_and_eat_breakfast' do
+      it 'tummy should be full' do
+        @new_human.goes_and_eat_breakfast
+        expect(@new_human.tummy).to eq('full')
+      end
     end
   end
 end
